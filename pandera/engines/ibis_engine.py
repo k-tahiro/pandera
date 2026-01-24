7614754cb29f3495b1c1bc342e79176ebfa815be
Deepyaman Datta <deepyaman.datta@utexas.edu>
2026-01-24 13:00:30 -0700

    Add map datatype to the Ibis engine implementation (#2206)
    * Add map datatype to the Ibis engine implementation
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    * Limit generic bases that get looked up in registry
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    * Revert to map implementation with default datatype
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    * Unit test equivalence of newly-added map datatypes
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    * Fix the module name lookup logic for generic bases
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    * Undo reversion to default datatypes implementation
    
    This reverts commit b058433cb0950752dc724282dc7ecb3981554348.
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    * Test checking all parametrizations of `Map` dtypes
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    * Add test cases corresponding to scenarios in issue
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    ---------
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- pandera/engines/ibis_engine.py
+++ pandera/engines/ibis_engine.py
@@ -58,7 +58,7 @@ class DataType(dtypes.DataType):
         data_container: ibis.Table | None = None,
     ) -> Union[bool, Iterable[bool]]:
         try:
-            return self.type == pandera_dtype.type
+            return self.type == Engine.dtype(pandera_dtype).type
         except TypeError:
             return False
 
@@ -484,3 +484,38 @@ class Timedelta(DataType, dtypes.DateTime):
         """Convert a :class:`dt.Interval` to a Pandera
         :class:`~pandera.engines.ibis_engine.Timedelta`."""
         return cls(unit=ibis_dtype.unit)
+
+
+###############################################################################
+# nested
+###############################################################################
+
+
+@Engine.register_dtype(
+    equivalents=[
+        dict,
+        dt.Map,
+    ]
+)
+@immutable(init=True)
+class Map(DataType):
+    """Semantic representation of a :class:`dt.Map`."""
+
+    type: dt.Map
+
+    def __init__(
+        self,
+        key_type: dt.DataType | None = None,
+        value_type: dt.DataType | None = None,
+    ):
+        if key_type is not None and value_type is not None:
+            object.__setattr__(self, "type", dt.Map(key_type, value_type))
+
+    @classmethod
+    def from_parametrized_dtype(cls, ibis_dtype: dt.Map):
+        """Convert a :class:`dt.Map` to a Pandera
+        :class:`~pandera.engines.ibis_engine.Map`."""
+        return cls(
+            key_type=ibis_dtype.key_type,
+            value_type=ibis_dtype.value_type,
+        )
